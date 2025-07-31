9c34cd494cacc0d5aac1b4ec75e6cce774d5ec70
dmreccentric <eccentricecc481@gmail.com>
2025-07-31 10:49:10 +0100

    adding the node-env to the .env

--- controllers/auth.js
+++ controllers/auth.js
@@ -46,7 +46,7 @@ const login = async (req, res) => {
     .cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "Strict" : "Lax",
+      sameSite: "Strict",
       maxAge: 60 * 60 * 1000, // 1 hour
     })
     .status(StatusCodes.OK)
